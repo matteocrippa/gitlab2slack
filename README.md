8b25963a5408cedbe973e3f8ae49907c93001a69
Matteo Crippa <github@matteocrippa.it>
2015-03-28 19:39:28 +0100

    basic structure for gateway

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gitlab2slack
 Gitlab 2 slack gateway
+
+## Usage
+
+I'd suggest to create an heroku instance, deploy it and once you have it working start using it like:
+
+http://URL/gateway?slack=<SLACKTOKEN>
---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express()
+var request = require('superagent')
+
+app.get('/gateway', function (req, res) {
+
+  var body = {
+    attachments: [
+      {
+        fallback: req.body.object_kind + ' on <' + req.body.repository.homepage + '|' + req.body.repository.name + '>',
+        pretext: req.body.object_kind + ' on <' + req.body.repository.homepage + '|' + req.body.repository.name + '>',
+        color: "#D00000"
+      }
+    ]
+  }
+
+  if(req.body.commits.length > 0){
+    body.attachments.fields.push({
+        title: 'Comment',
+        value: req.body.commits[0].message,
+        short: false
+      })
+  }
+
+  request
+    .post('https://hooks.slack.com/services/'+req.query.slack)
+    .send(body)
+    .end(function(err, resp){
+      if(err){
+        res.send(err)
+        }else{
+          res.send(resp)
+        }
+    })
+
+})
+
+app.listen(3000)
